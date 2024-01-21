9a1d19535f76b4e4d653d43b7ac1e6560bc70937
N-kapila <nirmalkapilarathne1998@gmail.com>
2024-01-22 00:35:34 +0530

    create doctor text fields

--- react-frontend/src/Components/Textfeilds.js
+++ react-frontend/src/Components/Textfeilds.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SmallTextField = ({ placeholder, value, onChange }) => {
   return (
@@ -7,7 +8,13 @@ const SmallTextField = ({ placeholder, value, onChange }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      style={{ width: "100px" }}
+      style={{
+        width: "100px",
+        height: "25px",
+        backgroundColor: "#EEEEEE",
+        borderRadius: "5px",
+        border: "0",
+      }}
     />
   );
 };
@@ -19,7 +26,14 @@ const MediumTextField = ({ placeholder, value, onChange }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      style={{ width: "200px" }}
+      style={{
+        width: "200px",
+        height: "25px",
+        backgroundColor: "#EEEEEE",
+        borderRadius: "5px",
+        border: "0",
+        padding: 5,
+      }}
     />
   );
 };
@@ -31,8 +45,55 @@ const LargeTextField = ({ placeholder, value, onChange }) => {
       placeholder={placeholder}
       value={value}
       onChange={onChange}
-      style={{ width: "300px" }}
+      style={{
+        width: "400px",
+        height: "20px",
+        backgroundColor: "#EEEEEE",
+        borderRadius: "10px",
+        border: "0",
+        padding: 10,
+      }}
     />
+  );
+};
+
+const PasswordInput = ({ placeholder, value, onChange }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        style={{
+          width: "400px",
+          height: "20px",
+          backgroundColor: "#EEEEEE",
+          borderRadius: "10px",
+          border: "0",
+          padding: 10,
+          marginBottom: "10px",
+        }}
+      />
+      <span
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: "10px",
+          transform: "translateY(-50%)",
+          cursor: "pointer",
+        }}
+        onClick={toggleShowPassword}
+      >
+        {showPassword ? <FaEye /> : <FaEyeSlash />}
+      </span>
+    </div>
   );
 };
 
@@ -51,4 +112,10 @@ const SelectTextField = ({ options, value, onChange, placeholder }) => {
   );
 };
 
-export { SmallTextField, MediumTextField, LargeTextField, SelectTextField };
+export {
+  SmallTextField,
+  MediumTextField,
+  LargeTextField,
+  PasswordInput,
+  SelectTextField,
+};
--- react-frontend/src/Pages/DoctorRegister.js
+++ react-frontend/src/Pages/DoctorRegister.js
@@ -1,9 +1,11 @@
-import React from "react";
 import img1 from "../assets/image1.png";
+import { LargeTextField, PasswordInput } from "../Components/Textfeilds";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 
 function DoctorRegister() {
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", height: "100%" }}>
       <div
         style={{
           justifyContent: "center",
@@ -13,7 +15,7 @@ function DoctorRegister() {
           width: "30%",
         }}
       >
-        <img src={img1} alt="Image Description" />
+        <img src={img1} alt="" />
       </div>
 
       <div
@@ -29,15 +31,118 @@ function DoctorRegister() {
             display: "flex",
             justifyContent: "flex-end",
             alignItems: "center",
-            padding: 15,
+            padding: 10,
           }}
         >
           <h4 style={{ marginRight: 5 }}>Already have an account?</h4>
           <a href="#">Sign in</a>
         </div>
 
-        <div style={{ justifyContent: "flex-start", alignItems: "flex-start" }}>
-          <h1 style={{ color: "var(--primary-color)" }}>Doctor Sign Up</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            paddingLeft: 15,
+          }}
+        >
+          <h1 style={{ color: "var(--primary-color)", fontSize: "50px" }}>
+            Doctor Sign Up
+          </h1>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ flex: 1, paddingLeft: 50 }}>
+            <div
+              style={{
+                alignItems: "flex-start",
+                display: "flex",
+                flexDirection: "column",
+                paddingLeft: 20,
+              }}
+            >
+              <p>Full Name:</p>
+              <LargeTextField placeholder={"Enter your name"} />
+              <br />
+              <p>Email:</p>
+              <LargeTextField placeholder={"Enter email address"} /> <br />
+              <p>Speciality:</p>
+              <LargeTextField placeholder={"Enter speciality"} /> <br />
+              <p>Password:</p>
+              <PasswordInput placeholder={"Enter password"} />
+            </div>
+          </div>
+
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                alignItems: "flex-start",
+
+                display: "flex",
+                flexDirection: "column",
+                paddingLeft: 30,
+              }}
+            >
+              <p>Doctor Id:</p>
+              <LargeTextField placeholder={"Enter your Id"} /> <br />
+              <p>Mobile Number:</p>
+              <LargeTextField placeholder={"Enter mobile number"} /> <br />
+              <p>Hospital Name:</p>
+              <LargeTextField placeholder={"Enter hospital name"} /> <br />
+              <p>Confirm Password:</p>
+              <PasswordInput placeholder={"Re-enter password"} />
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ flex: 1, paddingLeft: 50 }}>
+            <div
+              style={{
+                alignItems: "flex-start",
+                display: "flex",
+                flexDirection: "column",
+                paddingLeft: 20,
+              }}
+            >
+              {/* pofile pic */}
+            </div>
+          </div>
+
+          <div style={{ flex: 1 }}>
+            <div
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 50,
+                display: "flex",
+              }}
+            >
+              <button
+                style={{
+                  backgroundColor: "var(--primary-color)",
+                  color: "white",
+                  padding: "1px 50px 1px 50px",
+                  border: 0,
+                  borderRadius: "10px",
+                }}
+              >
+                {" "}
+                <h2> Sign Up</h2>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
