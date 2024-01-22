864bc1dc16d9c7d811667d6d8e269d7f141e2dfd
N-kapila <nirmalkapilarathne1998@gmail.com>
2024-01-23 00:03:27 +0530

    add mobile responsiveness

--- react-frontend/src/Pages/DoctorRegister.js
+++ react-frontend/src/Pages/DoctorRegister.js
@@ -1,7 +1,7 @@
 import img1 from "../assets/image1.png";
 import { LargeTextField, PasswordInput } from "../Components/Textfeilds";
 import { useState } from "react";
-import { FaUser } from "react-icons/fa";
+import "./DoctorRegisterStyles.css";
 
 function DoctorRegister() {
   const [profilePic, setProfilePic] = useState(null);
@@ -15,189 +15,87 @@ function DoctorRegister() {
   };
 
   return (
-    <div style={{ display: "flex", height: "100%", paddingBottom: 20 }}>
-      <div
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          display: "flex",
-          width: "30%",
-        }}
-      >
+    <div className="container">
+      <div className="container-one">
         <img src={img1} alt="" />
       </div>
 
-      <div
-        style={{
-          width: "70%",
-          borderLeft: "1px solid #ccc",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            padding: 10,
-          }}
-        >
+      <div className="container-two">
+        <div className="signin-header">
           <h4 style={{ marginRight: 5 }}>Already have an account?</h4>
           <a href="#">Sign in</a>
         </div>
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-            paddingLeft: 15,
-          }}
-        >
+        <div className="header">
           <h1 style={{ color: "var(--primary-color)", fontSize: "50px" }}>
             Doctor Sign Up
           </h1>
         </div>
 
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ flex: 1, paddingLeft: 50 }}>
-            <div
-              style={{
-                alignItems: "flex-start",
-                display: "flex",
-                flexDirection: "column",
-                paddingLeft: 20,
-              }}
-            >
-              <p>Full Name:</p>
-              <LargeTextField placeholder={"Enter your name"} />
-              <br />
-              <p>Email:</p>
-              <LargeTextField placeholder={"Enter email address"} /> <br />
-              <p>Speciality:</p>
-              <LargeTextField placeholder={"Enter speciality"} /> <br />
-              <p>Password:</p>
-              <PasswordInput placeholder={"Enter password"} />
-            </div>
+        <div className="textField-container">
+          <div className="textField-container-one">
+            <p>Full Name:</p>
+            <LargeTextField placeholder={"Enter your name"} />
+            <br />
+            <p>Doctor Id:</p>
+            <LargeTextField placeholder={"Enter your Id"} /> <br />
+            <p>Speciality:</p>
+            <LargeTextField placeholder={"Enter speciality"} /> <br />
           </div>
 
-          <div style={{ flex: 1 }}>
-            <div
-              style={{
-                alignItems: "flex-start",
-
-                display: "flex",
-                flexDirection: "column",
-                paddingLeft: 30,
-              }}
-            >
-              <p>Doctor Id:</p>
-              <LargeTextField placeholder={"Enter your Id"} /> <br />
-              <p>Mobile Number:</p>
-              <LargeTextField placeholder={"Enter mobile number"} /> <br />
-              <p>Hospital Name:</p>
-              <LargeTextField placeholder={"Enter hospital name"} /> <br />
-              <p>Confirm Password:</p>
-              <PasswordInput placeholder={"Re-enter password"} />
-            </div>
+          <div className="textField-container-one">
+            <p>Email:</p>
+            <LargeTextField placeholder={"Enter email address"} /> <br />
+            <p>Mobile Number:</p>
+            <LargeTextField placeholder={"Enter mobile number"} /> <br />
+            <p>Hospital Name:</p>
+            <LargeTextField placeholder={"Enter hospital name"} /> <br />
           </div>
         </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div style={{ flex: 1 }}>
-            <div
-              style={{
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-                paddingLeft: 20,
-              }}
-            >
-              {profilePic && (
-                <div>
-                  <div style={imageContainerStyle}>
-                    <img
-                      src={profilePic}
-                      alt="Profile"
-                      style={circleImageStyle}
-                    />
-                  </div>
+
+        <div className="textField-container">
+          <div className="textField-container-one">
+            <p>Password:</p>
+            <PasswordInput placeholder={"Enter password"} />
+          </div>
+
+          <div className="textField-container-one">
+            <p>Confirm Password:</p>
+            <PasswordInput placeholder={"Re-enter password"} />
+          </div>
+        </div>
+
+        <div className="containers">
+          <div className="profile-section">
+            {profilePic && (
+              <div>
+                <div className="profile-pic-container">
+                  <img src={profilePic} alt="Profile" className="profile-pic" />
                 </div>
-              )}
-              <div
-                style={{
-                  border: "1px solid black",
-                  padding: "2px",
-                  borderRadius: "5%",
-                }}
-              >
+              </div>
+            )}
+            <div>
+              <div className="label-container">
                 <input
                   type="file"
                   id="fileInput"
                   accept="image/*"
                   onChange={handleFileChange}
-                  style={{ display: "none" }}
                 />
                 <label htmlFor="fileInput">Choose profile picture</label>
               </div>
             </div>
           </div>
 
-          <div style={{ flex: 1 }}>
-            <div
-              style={{
-                justifyContent: "center",
-                alignItems: "center",
-                padding: 50,
-                display: "flex",
-              }}
-            >
-              <button
-                style={{
-                  backgroundColor: "var(--primary-color)",
-                  color: "white",
-                  padding: "1px 50px 1px 50px",
-                  border: 0,
-                  borderRadius: "10px",
-                }}
-              >
-                {" "}
-                <h2> Sign Up</h2>
-              </button>
-            </div>
+          <div className="button-section">
+            <button className="signup-button">
+              <h2>Sign Up</h2>
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-const imageContainerStyle = {
-  width: "150px",
-  height: "150px",
-  overflow: "hidden",
-  borderRadius: "50%",
-  padding: 20,
-};
-
-const circleImageStyle = {
-  width: "100%",
-  height: "100%",
-  objectFit: "cover",
-  borderRadius: "50%",
-};
 
 export default DoctorRegister;
