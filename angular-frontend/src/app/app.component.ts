c68e5df9a80508c687d4c9e4de193bfea46606a7
N-kapila <nirmalkapilarathne1998@gmail.com>
2024-02-06 20:26:21 +0530

    init

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextfieldsComponent } from '../components/textfields/textfields.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextfieldsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-frontend/src/components/textfields/textfields.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-textfields',
+  standalone: true,
+  imports: [],
+  templateUrl: './textfields.component.html',
+  styleUrl: './textfields.component.css'
+})
+export class TextfieldsComponent {
+
+}
